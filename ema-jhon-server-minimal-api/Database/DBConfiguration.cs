17b1c3e7c0b31270ff45e95b18e7de1720a95d62
Imamul Hassan <imamul.hassan@bjitgroup.com>
2022-04-27 12:28:00 +0600

    Database integration and settings.
    Signed-off-by: Imamul Hassan <imamul.hassan@bjitgroup.com>

---
+++ ema-jhon-server-minimal-api/Database/DBConfiguration.cs
@@ -0,0 +1,8 @@
+﻿namespace ema_jhon_server_minimal_api.Database
+{
+    public class DBConfiguration
+    {
+        public string ConnectionString { get; set; } = string.Empty;
+        public string DatabaseName { get; set; };
+    }
+}
--- ema-jhon-server-minimal-api/Program.cs
+++ ema-jhon-server-minimal-api/Program.cs
@@ -1,9 +1,12 @@
+using ema_jhon_server_minimal_api.Database;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.Configure<DBConfiguration>(builder.Configuration.GetSection("DatabaseSettings"));
 
 var app = builder.Build();
 
